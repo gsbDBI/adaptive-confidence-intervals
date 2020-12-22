f2d6646bd01cd75a911626b7e01048c0407fc860
halflearned <halflearned@gmail.com>
2020-12-22 14:00:01 -0800

    Adding sample mean back in

--- adaptive_CI/inference.py
+++ adaptive_CI/inference.py
@@ -64,11 +64,11 @@ def get_statistics(estimate, stderr, truth, ci_radius):
 def evaluate_aipw_stats(score, evalwts, truth, alpha=.1):
     estimate = np.sum(evalwts * score, 0) / np.sum(evalwts, 0)
     stderr = np.sqrt(np.sum(evalwts ** 2 * (score - estimate)** 2, 0)) / np.sum(evalwts, 0)
-    ci_radius =  norm.ppf(1.0 - alpha / 2) * stderr
+    ci_radius =  norm.ppf(1 - alpha / 2) * stderr
     return get_statistics(estimate, stderr, truth, ci_radius)
 
 
-def evaluate_sample_mean_stats(rewards, arms, truth, K, weights=None, alpha=0.10):
+def evaluate_sample_mean_naive_stats(outcomes, treatments, truth, K, weights=None, alpha=.1):
     estimate = np.empty(K)
     stderr = np.empty(K)
     ci_radius = np.empty(K)
@@ -77,6 +77,22 @@ def evaluate_sample_mean_stats(rewards, arms, truth, K, weights=None, alpha=0.10
         Tw = np.sum(treatments == w)
         estimate[w] = np.mean(y)
         stderr[w] = se = np.std(y) / np.sqrt(Tw)
+        ci_radius[w] = norm.ppf(1 - alpha/2) * se
+    return get_statistics(estimate, stderr, truth, ci_radius)  
+    
+
+def evaluate_sample_mean_naive_contrasts(outcomes, treatments, arm_truth, K, weights=None, alpha=.1):
+    estimate = np.empty(K-1)
+    stderr = np.empty(K-1)
+    ci_radius = np.empty(K-1)
+    y_comp = outcomes[treatments == K-1]
+    Tw_comp = np.sum(treatments == K-1)
+    truth = arm_truth[-1] - arm_truth[:-1]
+    for w in range(K-1):
+        y = outcomes[treatments == w]
+        Tw = np.sum(treatments == w)
+        estimate[w] = np.mean(y) - np.mean(y_comp)
+        stderr[w] = se = np.sqrt(np.var(y) / Tw + np.var(y_comp) / Tw_comp)
         ci_radius[w] = norm.ppf(1 - alpha/2) * se
     return get_statistics(estimate, stderr, truth, ci_radius)    
 
